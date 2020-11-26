670985d6354dea9e9bf7aea90446b659e2c14694
TheLittleBro122 <mynameiscutepanda@gmail.com>
2020-11-26 22:18:14 +0800

    displays the time on the web browser

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
-const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
+//Elements
+const countdownTimer = document.querySelector("#countdown-timer");
 
 let seconds = 5;
 let minutes = 0;
 let hours = 0;
 
-const countdown = setInterval(() => {
-    let timer = `${hours}:${minutes}:${seconds}`;
+//This makes the time pleasing to read
+const beautify = (hours, minutes, seconds) => {
+    seconds = seconds >= 0 && seconds <= 9 ? `0${seconds}` : seconds
+    minutes = minutes >= 0 && minutes <= 9 ? `0${minutes}` : minutes
+    hours = hours >= 0 && hours <= 9 ? `0${hours}` : hours
 
-    console.log(timer);
+    return `${hours}:${minutes}:${seconds}`;;
+}
+
+//Main countdown function
+const countdown = setInterval(() => {
+    let timer = beautify(hours, minutes, seconds);
+
+    countdownTimer.innerHTML = timer;
 
     if(hours === 0 && minutes === 0 && seconds === 0) {
         //This stops the timer everytime it's over
